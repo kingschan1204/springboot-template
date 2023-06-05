4d9038b4e6f3d0381058f256611247902f8f4848
kingschan <kings.chan@qq.com>
2023-06-05 11:52:10 +0800

    init

---
+++ standard/src/main/java/com/github/kingschan1204/standard/StandardApplication.java
@@ -0,0 +1,16 @@
+package com.github.kingschan1204.standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kingschan
+ */
+@SpringBootApplication
+public class StandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardApplication.class, args);
+    }
+
+}
